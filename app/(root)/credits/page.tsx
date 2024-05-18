de5e0288e8b37487603661eade093cc3f03f7a48
VarishTyagi <varishtyagi351@gmail.com>
2024-05-18 12:16:19 +0530

    initial commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreditPage = () => {
+  return (
+    <div> CreditsPage</div>
+  )
+}
+
+export default CreditPage
---
+++ app/(root)/transformation/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationPage</div>
+  )
+}
+
+export default TransformationsPage
